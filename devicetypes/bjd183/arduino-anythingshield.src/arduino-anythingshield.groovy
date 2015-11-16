637b61c59d11bbd4d39fc92a1db4cd814e95b533
Bryce Durham <bryce.j.durham@gmail.com>
2015-11-15 19:08:30 -0800

    Device type for Arduino ThingShield as currently configured.

---
+++ devicetypes/bjd183/arduino-anythingshield.src/arduino-anythingshield.groovy
@@ -0,0 +1,67 @@
+metadata {
+	definition (name: "Arduino AnyThingShield", namespace: "bjd183", author: "Bryce Durham") {
+		capability "Contact Sensor"
+//		capability "Sensor"
+
+		attribute "motionPin07", "string"
+		attribute "contactPin08", "string"
+		attribute "contactPin09", "string"
+		attribute "contactPin10", "string"
+		attribute "contactPin11", "string"
+		attribute "contactPin12", "string"
+	}
+
+    simulator {
+    }
+	
+    // Preferences
+
+	// tile definitions
+	tiles {
+        standardTile("motionPin07", "device.motionPin07", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state("active", label:'${name}', icon:"st.motion.motion.active", backgroundColor:"#ffa81e")
+			state("inactive", label:'${name}', icon:"st.motion.motion.inactive", backgroundColor:"#79b821")
+ 		}
+        standardTile("contactPin08", "device.contactPin08", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
+ 		}
+        standardTile("contactPin09", "device.contactPin09", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
+ 		}
+        standardTile("contactPin10", "device.contactPin10", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
+ 		}
+        standardTile("contactPin11", "device.contactPin11", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
+ 		}
+        standardTile("contactPin12", "device.contactPin12", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
+ 		}
+
+        main (["motionPin07"])
+        details(["motionPin07","contactPin08","contactPin09","contactPin10","contactPin11","contactPin12"])
+	}
+}
+
+def parse(String description) {
+    def msg = zigbee.parse(description)?.text
+    log.debug "Parse got '${msg}'"
+
+    def parts = msg.split(" ")
+    def name  = parts.length>0?parts[0].trim():null
+    def value = parts.length>1?parts[1].trim():null
+
+    name = value != "ping" ? name : null
+	
+    def result = createEvent(name: name, value: value, isStateChange: true)
+
+    log.debug result
+
+    return result
+}
+//no commands to implement for a read-only contact sensor
