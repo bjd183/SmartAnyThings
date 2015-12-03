e1d2d865268d13b1ef255a3f678ebc76a69cfa63
Bryce Durham <bryce.j.durham@gmail.com>
2015-12-02 20:01:31 -0800

    Change motion inactivity delay to 30 seconds.

--- Arduino/Sketches/SmartAnyThings/SmartAnyThings.ino
+++ Arduino/Sketches/SmartAnyThings/SmartAnyThings.ino
@@ -37,7 +37,7 @@ void setup()
   //when the input wire is connected to ground (interrupt==TRUE), the input pullup is bypassed and reads LOW
   static st::EX_Alarm2  executor1(F("alarmPin04"), 4, LOW, false, 360);
   
-  static st::IS2_Motion sensor1(F("motionPin07"), 7, HIGH, true, 250, 15);
+  static st::IS2_Motion sensor1(F("motionPin07"), 7, HIGH, true, 250, 30);
   static st::IS_Contact sensor2(F("contactPin08"), 8, LOW, true, 250);
   static st::IS_Contact sensor3(F("contactPin09"), 9, LOW, true, 250);
   static st::IS_Contact sensor4(F("contactPin10"), 10, LOW, true, 250);
